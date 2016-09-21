232d7261226cf5777ebedea840fbe28adf9713e2
Guido Maiola <guidomaiola@gmail.com>
2016-09-21 00:13:55 -0300

    First commit. Webapp skeleton. Adding Datatables library.

---
+++ Tomasulo_files/main.js
@@ -0,0 +1,11 @@
+
+
+
+$(document).ready( function () {
+    $('#table_instr').DataTable({
+    	searching: false,
+    	ordering:  false,
+    	lengthChange: false,
+    	autoWidth: false
+    });
+} );
