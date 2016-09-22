c66b92df8413da2dd152f96676a81e9dc1463208
Guido Maiola <guidomaiola@gmail.com>
2016-09-22 00:07:50 -0300

    Adding bootstrap. Adding dropdowns.

--- Tomasulo_files/main.js
+++ Tomasulo_files/main.js
@@ -2,10 +2,25 @@
 
 
 $(document).ready( function () {
-    $('#table_instr').DataTable({
-    	searching: false,
-    	ordering:  false,
-    	lengthChange: false,
-    	autoWidth: false
-    });
+
+
+	init();
+
+
+
+    
+
+	function init() {
+
+        $('#table_instr').DataTable({
+        	autoWidth: false,
+        	info: false,
+        	lengthChange: false,
+        	ordering:  false,
+        	pageLength: 5,
+        	paging: false,
+        	searching: false
+        });
+}
+
 } );
