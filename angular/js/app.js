10328affe05180fda65c91552950d9305f412dae
Guido Maiola <guido.maiola@globant.com>
2016-10-24 17:14:05 -0300

    Deleting main.js, adding to the angular app. Replacing datatables by bootstrap tables. Adding reset button logic.

--- angular/js/app.js
+++ angular/js/app.js
@@ -1,12 +1,17 @@
+
 var app = angular.module('tomasuloApp', []);
 
-
 app.controller('InstController', function() {
+
+	table_instr = $('#table_instr');
+	table_adder = $('#table_adder');
+	table_mult = $('#table_mult');
+	table_reg = $('#table_reg');
 
 	this.nInstruction = 1;
 
     this.defaultInst = {
-        type: 'INSTRUCTION',
+        type:'INSTRUCTION',
         dst: 'DST',
         op1: 'OP1',
         op2: 'OP2'
@@ -15,12 +20,12 @@ app.controller('InstController', function() {
     this.selectedInst = jQuery.extend(true, {}, this.defaultInst);
 
     this.instructions = [
-    {type: 'ADD', cycles: 1},
-    {type: 'SUBD', cycles: 1},
-    {type: 'MULD', cycles: 1},
-    {type: 'DIV', cycles: 1},
-    {type: 'ST', cycles: 1},
-    {type: 'LD', cycles: 1}];
+    {type:'ADD', cycles: 1},
+    {type:'SUBD', cycles: 1},
+    {type:'MULD', cycles: 1},
+    {type:'DIV', cycles: 1},
+    {type:'ST', cycles: 1},
+    {type:'LD', cycles: 1}];
 
     this.registers_bank = [
     {name: 'r0', value: '0'},
@@ -30,6 +35,36 @@ app.controller('InstController', function() {
     {name: 'r8', value: '0'}];
 
     this.instr_run = [];
+
+    this.setRegTable = function() {
+    	var defaultReg = [];
+    	defaultReg.push({number:'0', busy:'', tag:'', data:''});
+    	defaultReg.push({number:'2', busy:'', tag:'', data:''});
+    	defaultReg.push({number:'4', busy:'', tag:'', data:''});
+    	defaultReg.push({number:'6', busy:'', tag:'', data:''});
+    	defaultReg.push({number:'8', busy:'', tag:'', data:''});
+    	return defaultReg;
+    };
+    this.table_reg = this.setRegTable();
+
+    this.setAdderTable = function() {
+    	var defaultAdder = [];
+    	defaultAdder.push({inst:'', number:'1', tag1:'', op1:'', tag2:'', op2:''});
+    	defaultAdder.push({inst:'', number:'2', tag1:'', op1:'', tag2:'', op2:''});
+    	defaultAdder.push({inst:'', number:'3', tag1:'', op1:'', tag2:'', op2:''});
+    	return defaultAdder;
+    };
+    this.table_adder = this.setAdderTable();
+
+
+	this.setMulTable = function() {
+    	var defaultMul = [];
+    	defaultMul.push({inst:'', number:'1', tag1:'', op1:'', tag2:'', op2:''});
+    	defaultMul.push({inst:'', number:'2', tag1:'', op1:'', tag2:'', op2:''});
+    	return defaultMul;
+    };
+    this.table_mult = this.setMulTable();
+
 
     this.select = function(field, instruction) {
         this.selectedInst[field] = instruction;
@@ -94,4 +129,41 @@ app.controller('InstController', function() {
     	this.updateBtns();
     };
 
+    /**
+    * Set method: 
+    *   Disable "Instructions", "Cicles" and "Controls" button groups.
+    *   Enable button "RUN CYCLE"
+    */
+    this.set = function() {
+
+            var btnGrpAdd = $('.btn-grp-add-inst');
+            btnGrpAdd.find("button").addClass("disabled");
+
+            var ciclePerInst = $('.cicle-per-instructions');
+            ciclePerInst.find("button").addClass("disabled");
+
+            var btnRun = $('#btn-run');
+            if (btnRun.hasClass("disabled")){
+                    btnRun.removeClass("disabled");
+            }
+    };
+
+
+    /**
+    * Reset method: Re-enable "Instructions", "Cicles" and "Controls" button groups. Delete tables data.
+    */
+    this.reset = function() {
+            $('.btn-grp-add-inst').find("button").removeClass("disabled");
+            $('.cicle-per-instructions').find("button").removeClass("disabled");
+            $('#btn-run').addClass("disabled");
+
+            this.instr_run = [];
+            this.table_reg = this.setRegTable();
+            
+    };
+
+    
+
 });
+
+
