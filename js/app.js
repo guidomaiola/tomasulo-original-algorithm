869e4718ef7d094517e0b6aba7f30fd4f85a67a3
Guido Maiola <guidomaiola@gmail.com>
2017-02-15 01:27:31 -0300

    Adding RUN logic.

--- js/app.js
+++ js/app.js
@@ -17,15 +17,16 @@ app.controller('InstController', function() {
         op2: 'OP2'
     };
 
+    // Se asigna el valor de la instruccion 'default' (defaultInst)
     this.selectedInst = jQuery.extend(true, {}, this.defaultInst);
 
     this.instructions = [
-    {type:'ADD', cycles: '#cycles'},
-    {type:'SUBD', cycles: '#cycles'},
-    {type:'MULD', cycles: '#cycles'},
-    {type:'DIV', cycles: '#cycles'},
-    {type:'ST', cycles: '#cycles'},
-    {type:'LD', cycles: '#cycles'}];
+    {type:'ADD', cycles: 1},
+    {type:'SUBD', cycles: 1},
+    {type:'MULD', cycles: 1},
+    {type:'DIV', cycles: 1},
+    {type:'ST', cycles: 1},
+    {type:'LD', cycles: 1}];
 
     this.instructionsCycles = ['1', '2', '3', '4', '5', '6', '7', '8'];
 
@@ -40,32 +41,48 @@ app.controller('InstController', function() {
 
     this.setRegTable = function() {
     	var defaultReg = [];
-    	defaultReg.push({number:'0', busy:'', tag:'', data:''});
-    	defaultReg.push({number:'2', busy:'', tag:'', data:''});
-    	defaultReg.push({number:'4', busy:'', tag:'', data:''});
-    	defaultReg.push({number:'6', busy:'', tag:'', data:''});
-    	defaultReg.push({number:'8', busy:'', tag:'', data:''});
+    	defaultReg.push({number:'0', busy:'', tag:'', data:''}); // pos 0
+    	defaultReg.push({number:'2', busy:'', tag:'', data:''}); // pos 1
+    	defaultReg.push({number:'4', busy:'', tag:'', data:''}); // pos 2
+    	defaultReg.push({number:'6', busy:'', tag:'', data:''}); // pos 3
+    	defaultReg.push({number:'8', busy:'', tag:'', data:''}); // pos 4
     	return defaultReg;
     };
     this.table_reg = this.setRegTable();
 
-    this.setAdderTable = function() {
-    	var defaultAdder = [];
-    	defaultAdder.push({inst:'', number:'1', tag1:'', op1:'', tag2:'', op2:''});
-    	defaultAdder.push({inst:'', number:'2', tag1:'', op1:'', tag2:'', op2:''});
-    	defaultAdder.push({inst:'', number:'3', tag1:'', op1:'', tag2:'', op2:''});
-    	return defaultAdder;
+
+    this.addToAdder = function(instr) {
+    	var instructionToAdd = instr;
+
+        // si es raw la funcion le devuelve el valor del tag, segun la dependencia, para el op1
+        var tag = isRAW(instructionToAdd,1);
+        instructionToAdd['tag1'] = tag;
+
+        // si es raw la funcion le devuelve el valor del tag, segun la dependencia, para el op2
+        tag = isRAW(instructionToAdd,2);
+        instructionToAdd['tag2'] = tag;
+
+        // lo debería agregar en el primer slot disponible
+    	this.table_adder.push(instructionToAdd);
     };
-    this.table_adder = this.setAdderTable();
+    this.table_adder = [];
 
 
-	this.setMulTable = function() {
-    	var defaultMul = [];
-    	defaultMul.push({inst:'', number:'1', tag1:'', op1:'', tag2:'', op2:''});
-    	defaultMul.push({inst:'', number:'2', tag1:'', op1:'', tag2:'', op2:''});
-    	return defaultMul;
+	this.addToMul = function(instr) {
+    	var instructionToMul = instr;
+
+        // si es raw la funcion le devuelve el valor del tag, segun la dependencia, para el op1
+        var tag = isRAW(instructionToMul,1);
+        instructionToMul['tag1'] = tag;
+
+        // si es raw la funcion le devuelve el valor del tag, segun la dependencia, para el op2
+        tag = isRAW(instructionToMul,2);
+        instructionToMul['tag2'] = tag;
+
+        // REEMPLAZAR: lo debería agregar en el primer slot disponible
+        this.table_mult.push(instructionToMul);
     };
-    this.table_mult = this.setMulTable();
+    this.table_mult = [];
 
 
     this.select = function(field, instruction) {
@@ -85,7 +102,7 @@ app.controller('InstController', function() {
 	this.resetInstCycles = function() {
 		var instArray = this.instructions;
 		$.each(instArray, function() {
-        	this.cycles = '#cycles';
+        	this.cycles = 1;
     	});
 	};
 
@@ -154,16 +171,20 @@ app.controller('InstController', function() {
     */
     this.set = function() {
 
-            var btnGrpAdd = $('.btn-grp-add-inst');
-            btnGrpAdd.find("button").addClass("disabled");
+        
+        var btnGrpAdd = $('.btn-grp-add-inst');
+        btnGrpAdd.find("button").addClass("disabled");
 
-            var ciclePerInst = $('.cicle-per-instructions');
-            ciclePerInst.find("button").addClass("disabled");
+        var ciclePerInst = $('.cicle-per-instructions');
+        ciclePerInst.find("button").addClass("disabled");
 
-            var btnRun = $('#btn-run');
-            if (btnRun.hasClass("disabled")){
-                    btnRun.removeClass("disabled");
+        var btnRun = $('#btn-run');
+        if (btnRun.hasClass("disabled")){
+            if (this.keepRunning()) {
+                btnRun.removeClass("disabled");
             }
+        }
+            
     };
 
 
@@ -179,6 +200,80 @@ app.controller('InstController', function() {
             this.table_reg = this.setRegTable();
             this.resetInstCycles();
     };
+
+
+    // agrego la instruccion a la ER correspondiente
+    this.dispatch = function(inst) {
+
+        switch(inst.type) {
+            case 'ADD':
+                this.addToAdder(inst);
+                break;
+            case 'SUBD':
+                this.addToAdder(inst);
+                break;
+            case 'MULD':
+                this.addToMul(inst);
+                break;
+            case 'DIV':
+                this.addToMul(inst);
+                break;
+            case 'ST':
+                /**code block*/
+                break;
+            case 'LD':
+                /**code block*/
+                break;
+        }
+
+    };
+
+
+    this.execute = function(time) {
+
+        if (!raw(this.table_adder)) {
+
+        }
+
+        if (!raw(this.table_mult)) {
+
+        }
+
+        // guardo el tiempo en que se empezó a ejecutar
+        instructionToAdd['time'] = time;
+
+    };
+
+    // Da TRUE siempre que haya algo para ejecutar o despachar.
+    this.keepRunning = function() {
+        return ((this.instr_run.length != 0) || (this.table_adder.length != 0) || (this.table_mult.length != 0))
+    };
+
+
+    this.time = 0;
+
+    this.run = function() {
+
+        this.time= this.time + 1;
+
+        // si la lista de instrucciones no esta vacia, despacho.
+        if (this.instr_run.length > 0 ) {
+
+            // tomo la primer instruccion de la lista
+            var inst = this.instr_run.shift();
+        
+            // la despacho a la ER correspondiente
+            this.dispatch(inst);
+        }
+
+            
+
+        // ejecuto lo que haya por ejecutar
+        this.execute(time);
+            
+            
+    };
+
 
     
 
